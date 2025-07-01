f4ac9086c7aaedbc3a56c1535d8d5a73b35d3881
Paarisha1 <147527266+Paarisha1@users.noreply.github.com>
2025-07-01 13:26:53 +0530

    Update CustomerDetail.controller.js

--- webapp/controller/CustomerDetail.controller.js
+++ webapp/controller/CustomerDetail.controller.js
@@ -7,35 +7,35 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 
     onInit: function () {
      
-      var oRouter = this.getOwnerComponent().getRouter(); // Get app router
+      var oRouter = this.getOwnerComponent().getRouter(); 
       oRouter
-        .getRoute("CustomerDetail") // Access 'CustomerDetail' route
-        .attachPatternMatched(this._onObjectMatched, this); // Call function when route matches
+        .getRoute("CustomerDetail") 
+        .attachPatternMatched(this._onObjectMatched, this); 
     },
 
     _onObjectMatched: function (oEvent) {
-      // Triggered by matched route
-      var sCustomerId = oEvent.getParameter("arguments").customerId; // Get customerId from URL
+    
+      var sCustomerId = oEvent.getParameter("arguments").customerId; 
       this.getView().bindElement({
-        // Bind view to OData path
-        path: "/Customers('" + sCustomerId + "')", // Bind to specific customer
+       
+        path: "/Customers('" + sCustomerId + "')", 
       });
     },
 
     onOrdersPress: function () {
-      // When 'Show Orders' button clicked
-      var sCustomerId = this.getView() // Access current view
-        .getBindingContext() // Get data context
-        .getProperty("CustomerID"); // Get customer ID from data
+     
+      var sCustomerId = this.getView()
+        .getBindingContext() 
+        .getProperty("CustomerID"); 
       this.getOwnerComponent().getRouter().navTo("CustomerOrders", {
-        // Navigate to 'CustomerOrders' view
-        customerId: sCustomerId, // Pass customerId
+      
+        customerId: sCustomerId,
       });
     },
 
     onNavBack: function () {
-      // When back button is pressed
-      history.go(-1); // Go back one page in browser history
+
+      history.go(-1);
     },
   });
 });
